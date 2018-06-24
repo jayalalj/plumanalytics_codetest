f819776dac08223dc54cfce70246c3aebb439ff0
Janaka Jayalal <jjayalal@seic.com>
2018-06-23 23:34:53 -0400

    adding looping and result comparing logics to compare multiple execution results

--- src/java/com/plumanalytics/codetest/MetricProcessor.java
+++ src/java/com/plumanalytics/codetest/MetricProcessor.java
@@ -4,10 +4,14 @@ import java.io.*;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import com.plumanalytics.codetest.TestMetricPublisher.CountInstance;
 
 /**
  * Process metric files using a thread pool
@@ -18,17 +22,65 @@ public class MetricProcessor {
   MetricPublisher publisher = new TestMetricPublisher();
 
   public static void main(String args[]) {
-    try {
-      URL url = MetricProcessor.class.getResource("/test-data");
-      File testDataDir = new File(url.toURI());
-      MetricProcessor processor = new MetricProcessor(testDataDir);
-      processor.run();
-    } catch (Throwable e) {
-      e.printStackTrace();
-      return;
-    }
-    System.out.println("!!--code executed without exception---!!");
+	  Map<Date, Map<String, CountInstance>> dateMapCurr=null, dateMapPrev =null;
+	  Map<String, CountInstance> countMapByIdCurr=null, countMapByIdPrv=null;
+	    try {
+				for (int i = 0; i < 2; i++) {
+					URL url = MetricProcessor.class.getResource("/test-data");
+					File testDataDir = new File(url.toURI());
+					MetricProcessor processor = new MetricProcessor(testDataDir);
+					processor.run();
+					dateMapPrev = dateMapCurr;
+					dateMapCurr = processor.publisher.getMetricMapByDate();
+					countMapByIdPrv = countMapByIdCurr;
+					countMapByIdCurr = processor.publisher.getAggregateCountMapById();
+					if(i>0){
+					  System.out.println("--validating validateConsistancyDateMap -- ");
+					  validateConsistancyDateMap(dateMapCurr, dateMapPrev);
+					  System.out.println("--validating validateConsistancyDateMap -- ");
+					  validateCountInstMap(countMapByIdCurr, countMapByIdPrv);
+					}
+				}
+	      
+	    } catch (Throwable e) {
+	      e.printStackTrace();
+	      throw new RuntimeException(e);
+	    }
+	    
+	    System.out.println("!!! Go Eagles !!!");
   }
+  
+  private static void validateConsistancyDateMap(Map<Date, Map<String, CountInstance>> dateMapCurr,
+			Map<Date, Map<String, CountInstance>> dateMapPrev) {
+		for (Date dt : dateMapCurr.keySet()) {
+	    	if(!dateMapPrev.containsKey(dt)){
+	    		System.out.println("broken, date map values not match-----"+dt);
+	    		throw new RuntimeException("broken, date map values not match-----"+dt);
+	    	}
+	    	
+	        //validate inner maps	
+	    	Map<String, CountInstance> curInstMap  = dateMapCurr.get(dt);
+    		Map<String, CountInstance> prvInstMap  = dateMapPrev.get(dt);
+    		validateCountInstMap(curInstMap, prvInstMap);
+	    		
+		}
+	}
+  
+  private static void validateCountInstMap(Map<String, CountInstance> curInstMap, Map<String, CountInstance> prvInstMap) {
+		for (String key : curInstMap.keySet()) {
+			if(!prvInstMap.containsKey(key)){
+				System.out.println("broken, Incetance map keys not mached");
+				throw new RuntimeException("broken, Incetance map keys not mached");
+			}
+			if(!prvInstMap.get(key).equals(curInstMap.get(key))){
+				System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%    broken, Incetance map values not match-----"+key);
+				System.out.println(prvInstMap.get(key).toString());
+				System.out.println(curInstMap.get(key).toString());
+				throw new RuntimeException("broken, Incetance map values not match-----"+key);
+			}
+		}
+	}
+
 
   private MetricProcessor(File sourceDir) {
     this.sourceDir = sourceDir;
--- src/java/com/plumanalytics/codetest/MetricPublisher.java
+++ src/java/com/plumanalytics/codetest/MetricPublisher.java
@@ -1,6 +1,10 @@
 package com.plumanalytics.codetest;
 
 import java.text.ParseException;
+import java.util.Date;
+import java.util.Map;
+
+import com.plumanalytics.codetest.TestMetricPublisher.CountInstance;
 
 /**
  * Publish Metrics
@@ -9,5 +13,7 @@ public interface MetricPublisher {
 
   public MetricMessage createMessage(String line) throws ParseException;
   public void publishMetric(MetricMessage metricMessage);
+  public Map<Date, Map<String, CountInstance>> getMetricMapByDate();
+  public Map<String, CountInstance> getAggregateCountMapById();
 
 }
